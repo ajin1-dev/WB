4aafab8ec11a4b072d883dfa5ea55584f8190701
Adrian Jin <ajin1@worldbank.org>
2026-06-10 13:42:10 -0400

    Visualizations for PoU data and FIES food insecurity data

---
+++ Somalia_Hunger/Scripts/hunger_visualizations.R
@@ -0,0 +1,113 @@
+library(tidyverse)
+library(readxl)
+
+
+## vectors for benchmarks and makeshift benchmarks
+benchmarks = c("SOM", "SSF", "LIC", "WLD", "FCS")
+classifications <- read_excel("Somalia_Hunger/Data/country_classifications.xlsx")
+low_inc_codes <- classifications |>
+  filter(`Income group` == "Low income") |>
+  pull(Code)
+ssa_codes <- classifications |>
+  filter(Region == "Sub-Saharan Africa") |>
+  pull(Code)
+
+# Load in PoU dataset
+## Prevalence of undernourishment (% of population); ind = SN.ITK.DEFC.ZS
+pou <- read.csv("Somalia_Hunger/Data/API_SN.ITK.DEFC.ZS_DS2_en_csv_v2_331187.csv", skip = 4) |>
+  select(c(Country.Name, Country.Code, "X2001":last_col(offset = 3))) |>
+  filter(Country.Code %in% benchmarks)
+colnames(pou) <- gsub("X", "", colnames(pou))
+pou <- pou |>
+  pivot_longer(cols = "2001":last_col(),
+               names_to = "year",
+               values_to = "value")
+
+# Create PoU visualization
+pou_plot <- pou |> ggplot(aes(x = year, y = value, group = Country.Name, color = Country.Name)) +
+  geom_line() +
+  scale_x_discrete(breaks = seq(2001, 2023, by = 5)) +
+  labs(title = "Prevalence of Undernourishment (% of population)",
+       x = "Year",
+       y = "% Undernourishment",
+       color = "Country/Grouping")
+ggsave("Somalia_Hunger/Visualizations/Pct_Undernourishment.png")
+
+
+
+# Load in FIES Dataset
+## Prevalence of moderate or severe food insecurity in the population (%); ind = SN.ITK.MSFI.ZS
+insecurity_ms<- read.csv("Somalia_Hunger/Data/API_SN.ITK.MSFI.ZS_DS2_en_csv_v2_7955.csv", skip = 4) |>
+  select(c(Country.Name, Country.Code, "X2021")) |>
+  rename(pct = X2021)
+
+## Creating Makeshift Benchmarks
+### world average
+wld_avg_ms <- insecurity_ms |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "World", Country.Code = "WLD")
+### ssa average
+ssa_avg_ms <- insecurity_ms |>
+  filter(Country.Code %in% ssa_codes) |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "Sub-Saharan Africa", Country.Code = "SSF")
+### low income average
+lic_avg_ms <- insecurity_ms |>
+  filter(Country.Code %in% low_inc_codes) |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "Low income", Country.Code = "LIC")
+### Joining to main dataset and filtering
+insecurity_ms <- insecurity_ms |>
+  filter(Country.Code == "SOM") |>
+  bind_rows( wld_avg_ms, ssa_avg_ms, lic_avg_ms)
+  
+
+## Prevalence of severe food insecurity in the population (%); ind = SN.ITK.SVFI.ZS
+insecurity_s <- read.csv("Somalia_Hunger/Data/API_SN.ITK.SVFI.ZS_DS2_en_csv_v2_331188.csv", skip = 4) |>
+  select(c(Country.Name, Country.Code, Indicator.Name, "X2021")) |>
+  rename(pct = X2021)
+
+## Creating Makeshift Benchmarks
+### world average
+wld_avg_s <- insecurity_s |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "World", Country.Code = "WLD")
+### ssa average
+ssa_avg_s <- insecurity_s |>
+  filter(Country.Code %in% ssa_codes) |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "Sub-Saharan Africa", Country.Code = "SSF")
+### low income average
+lic_avg_s <- insecurity_s |>
+  filter(Country.Code %in% low_inc_codes) |>
+  summarize(pct = mean(pct, na.rm = TRUE)) |>
+  mutate(Country.Name = "Low income", Country.Code = "LIC")
+### Joining to main dataset and filtering
+insecurity_s <- insecurity_s |>
+  filter(Country.Code == "SOM") |>
+  bind_rows( wld_avg_s, ssa_avg_s, lic_avg_s)
+
+
+# Create FIES visualizations
+modsev_plot <- insecurity_ms |>
+  ggplot(aes(x = Country.Name, y = pct, fill = Country.Name)) +
+  geom_col() +
+  labs(
+    title = "2021: Prevalence of moderate and severe food insecurity in the population (%)",
+    x = "Country/Grouping",
+    y = "% of population insecure",
+    fill = "Country/Grouping"
+  )
+
+sev_plot <- insecurity_s |>
+  ggplot(aes(x = Country.Name, y = pct, fill = Country.Name)) +
+  geom_col() +
+  labs(
+    title = "2021: Prevalence of severe food insecurity in the population (%)",
+    x = "Country/Grouping",
+    y = "% of population insecure",
+    fill = "Country/Grouping"
+  )
+
+modsev_plot
+sev_plot
